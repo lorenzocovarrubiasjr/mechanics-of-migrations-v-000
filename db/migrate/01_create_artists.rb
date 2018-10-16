d589b216197105a5a777fb5db18860a5175d4b19
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2018-10-16 19:52:55 +0000

    Done.

---
+++ db/migrate/01_create_artists.rb
@@ -0,0 +1,18 @@
+class CreateArtists < ActiveRecord::Migration
+  def up 
+  end 
+  
+  def down
+  end 
+  
+  def change
+    create_table :artist do
+      |t|
+        t.string :name 
+        t.string :genre 
+        t.integer :age 
+        t.string :hometown
+    end 
+  end 
+  
+end 
---
+++ db/migrate/02_add_favorite_food_to_artist.rb
@@ -0,0 +1,6 @@
+class AddFavoriteFoodToArtist < ActiveRecord::Migration 
+  def change
+    add_column :artists,
+    :favorite_food, :string 
+  end 
+end 
